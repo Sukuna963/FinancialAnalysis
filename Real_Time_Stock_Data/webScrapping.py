8ac588ec3e3fb7b768b93f7b7dbf0cceed297abb
Sukuna963 <86301498+Sukuna963@users.noreply.github.com>
2024-02-26 17:22:32 -0300

    *

---
+++ Real_Time_Stock_Data/webScrapping.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import datetime
+import requests
+from requests.exceptions import ConnectionError
+from bs4  import BeautifulSoup
+
+
+def web_content_div(web_content, class_path):
+    web_content_div = web_content.find_all('div', {'class': class_path})
+
+    try:
+        spans = web_content_div[0].find_all('span')
+        texts = [span.get_text() for span in spans]
+    except IndexError:
+        texts = []
+
+    return texts
+
+def real_time_price(stock_code):
+    url = 'https://finance.yahoo.com/quote/' + stock_code + '?.tsrc=fin-srch'
+    
+    try:
+        r = requests.get(url)
+        web_content = BeautifulSoup(r.text, 'html.parser')
+        texts = web_content_div(web_content, 'My(6px) Pos(r) smartphone_Mt(6px)')
+        if texts != []:
+            price, change = texts[0], texts[1]
+        else:    
+            price, change = [], []
+
+    except ConnectionError:
+        price, change = [], []
+
+    return texts
+
+print(real_time_price('AMZN'))
